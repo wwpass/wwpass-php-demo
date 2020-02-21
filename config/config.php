48988a1a720b88b01be4f4c568c3b49663968a18
Mikhail Vysogorets <m.vysogorets@wwpass.com>
2020-02-21 18:13:23 +0000

    initial commit

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('WWPASS_CERT_FILE', "/etc/ssl/example.org.crt");
+define('WWPASS_KEY_FILE', "/etc/ssl/example.org.key");
+define('WWPASS_CA_FILE', "/etc/ssl/wwpass_sp_ca.crt");
+define('WWPASS_PIN_REQUIRED', false);
